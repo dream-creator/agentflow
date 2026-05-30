62c5a84ae95b9d2412566944e3edb90373472f9e
Ryan Gabriel <ryandasalla1001@gmail.com>
2026-05-30 18:53:31 +0800

    feat: add data-fetching hooks (useLeads, useProfile, useActions)
    - Create useLeads hook (fetchLeads, createLead, updateLead, deleteLead)
    - Create useProfile hook (fetchProfile, updateProfile)
    - Create useActions hook (fetchActions, createAction, completeAction)
    - Add 9 unit tests following TDD workflow
    - All 118 tests passing, lint clean, build successful

---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export { fetchLeads, createLead, updateLead, deleteLead } from "./useLeads";
+export { fetchProfile, updateProfile } from "./useProfile";
+export { fetchActions, createAction, completeAction } from "./useActions";
